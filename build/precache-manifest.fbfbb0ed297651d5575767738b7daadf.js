1e9a2f3a5fc09d58307cdf83fa7dc99e4dfa2782
Fernanda Mendes <fernanda.mendes@usemobile.xyz>
2021-03-05 11:14:38 -0300

    deploy production

---
+++ build/precache-manifest.fbfbb0ed297651d5575767738b7daadf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b7331735cd6e71ce35f1f5f008bbde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e611f8c7fc46046bdfc",
+    "url": "/static/css/2.ba57f93f.chunk.css"
+  },
+  {
+    "revision": "1a2016cbb04d9e780d07",
+    "url": "/static/css/main.c9efb1c3.chunk.css"
+  },
+  {
+    "revision": "3e611f8c7fc46046bdfc",
+    "url": "/static/js/2.804e4502.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.804e4502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2016cbb04d9e780d07",
+    "url": "/static/js/main.87a57547.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
