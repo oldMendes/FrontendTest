c3ef3362e901da9fd5ed99a23c8c9bf8a3da4a76
Fernanda Mendes <fernanda.mendes@usemobile.xyz>
2021-03-05 14:59:29 -0300

    Remove node_modules

--- node_modules/antd-mask-input/.storybook/config.js
+++
@@ -1,5 +0,0 @@
-import '@storybook/addon-console';
-import { configure } from '@storybook/react';
-
-// automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.(js|ts|tsx)$/), module);
